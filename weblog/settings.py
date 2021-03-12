d4b9b061deca8a35ac2254b1d22e505196a95c4c
Shayan-9248 <shayan.aimoradii@gmail.com>
2021-03-12 19:42:56 +0330

    session authentication drf

--- weblog/settings.py
+++ weblog/settings.py
@@ -208,8 +208,8 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.AllowAny',
     # )
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.TokenAuthentication',
     ]
 }
