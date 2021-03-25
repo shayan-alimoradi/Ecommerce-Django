2a57bd12a706469dc9b4c95cf3a261b77343d512
Shayan-9248 <shayan.aimoradii@gmail.com>
2021-03-25 07:40:17 +0430

    testing account views

---
+++ shop_account/tests/test_views.py
@@ -0,0 +1,50 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from shop_account.models import *
+from shop_account.forms import *
+
+
+class TestView(TestCase):
+    def setup(self):
+        self.client  = Client()
+    
+    def test_user_signup_GET(self):
+        response = self.client.get(reverse('account:sign-up'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/sign_up.html')
+        self.failUnless(response.context['form'], SignUpForm)
+    
+    def test_user_signup_POST_valid(self):
+        response = self.client.post(reverse('account:sign-up'), data={
+            'username': 'anna',
+            'email': 'anna@email.com',
+            'password': '123',
+            'confirm_password': '123'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(Profile.objects.count(), 1)
+
+    def test_user_signup_POST_invalid(self):
+        response = self.client.post(reverse('account:sign-up'), data={
+            'username': 'jack',
+            'email': 'invalidemail',
+            'password': '123',
+            'confirm_password': '123'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.failIf(response.context['form'].is_valid())
+        self.assertFormError(response, 'form', field='email', errors=['please enter a valid email address'])
+
+    # def test_user_profile_GET(self):
+    #     self.client.login(email='max@email.com', password='max123')
+    #     response = self.client.get(reverse('account:profile'))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed('account/profile.html')
+
+    def test_user_signin_GET(self):
+        response = self.client.get(reverse('account:sign-in'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('account/sign_in.html')
+        self.failUnless(response.context['form'], SignInForm)
+        
