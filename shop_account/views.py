dbee1d87ff101acf2b2fc2bed3a10c592c7fb8a4
Shayan-9248 <74461630+Shayan-9248@users.noreply.github.com>
2021-06-21 22:07:10 +0430

    Update views.py

--- shop_account/views.py
+++ shop_account/views.py
@@ -1,11 +1,10 @@
+# Standard-library import
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.template.loader import render_to_string
-from .tokens import account_activation_token
 from django.views.generic.edit import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
@@ -15,8 +14,15 @@ from django.contrib import messages
 from django.core.mail import EmailMessage
 from django.urls import reverse, reverse_lazy
 from django.views import View
-from shop_order.models import *
-from .forms import *
+
+# Local import
+from shop_order.models import OrderItem
+from .tokens import account_activation_token
+from .forms import (
+    SignInForm,
+    SignUpForm,
+    ProfileForm,
+)
 
 
 class SignIn(View):
